e6a323aa71f3654481cbd0230fd7465e7c16c953
JoeKaleli <kalelimburu9@gmail.com>
2023-12-14 17:37:31 +0300

    Added some comments to clarify my code

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const max =Math.floor(Math.max(20,16,17,2));
+const max =Math.floor(Math.max(20,16,17,2));//Created the variable max that generates the highest constituent
  
  let num1 = 31;
  let num2 = 2;
- var multiply = num1*num2;
+ var multiply = num1*num2;//Create the variable multiply with an equation that multiplies two consecutive numbers
  
- const random = Math.round(Math.random()*10) +1;
+ const random = Math.round(Math.random()*10) +1;// Created the variable random that generates a random integer greater than 0
 
  const num3 =17;
  const num4 =4;
- const mod = Math.floor(num3/num4);
-
- console.log(mod)
+ const mod = Math.floor(num3/num4);// Create the variable mod that will calculate the remainder of two  numbers and be 4
 
  

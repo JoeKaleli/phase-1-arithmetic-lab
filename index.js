ce60b66e4992c7a3cc79c4f93241c7ec316c7c62
JoeKaleli <kalelimburu9@gmail.com>
2023-12-14 16:57:51 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const max =Math.floor(Math.max(20,16,17,2));
+ 
+ let num1 = 31;
+ let num2 = 2;
+ var multiply = num1*num2;
+ 
+ const random = Math.round(Math.random()*10) +1;
 
-// Write your code here
+ const num3 =17;
+ const num4 =4;
+ const mod = Math.floor(num3/num4);
+
+ console.log(mod)
+
+ 
